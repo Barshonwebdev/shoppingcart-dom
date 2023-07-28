3a42e7abaafd84e1f0e513e846596efeb5a7b61a
Barshon <barshonweb@gmail.com>
2023-07-28 08:03:12 +0600

    item calculation

--- js/case.js
+++ js/case.js
@@ -1,7 +1,30 @@
-document.getElementById("case-plus").addEventListener("click",function(){
-    const caseNumberElement=document.getElementById("case-number");
-    const caseNumberElementString=caseNumberElement.value;
-    const previousCaseValue=parseInt(caseNumberElementString);
-    const newCaseValue=previousCaseValue+1;
-    caseNumberElement.value=newCaseValue;
-})
+function increaseOrDecreaseItem(elementId,currentNumber,currentPrice){
+    document.getElementById(elementId).addEventListener("click", function () {
+      const NumberElement = document.getElementById(currentNumber);
+      const NumberElementString = NumberElement.value;
+      const previousValue = parseInt(NumberElementString);
+      const priceElement = document.getElementById(currentPrice);
+      const priceElementString=priceElement.innerText;
+      const previousPrice=parseFloat(priceElementString);
+      if(elementId=="case-plus" || elementId=="phone-plus"){
+        const newValue = previousValue + 1;
+        NumberElement.value = newValue;
+        const newPrice=previousPrice * newValue;
+        priceElement.innerText=newPrice;
+      } else{
+        const newValue = previousValue - 1;
+        NumberElement.value = newValue;
+        const newPrice = previousPrice / newValue;
+        priceElement.innerText = newPrice;
+        if(NumberElement.value<0){
+            alert("please select positive amount of items")
+            NumberElement.value=0;
+        }
+      }
+    });
+}
+
+increaseOrDecreaseItem("case-plus","case-number","case-price");
+increaseOrDecreaseItem("phone-plus","phone-number","phone-price");
+increaseOrDecreaseItem("case-minus","case-number","case-price");
+increaseOrDecreaseItem("phone-minus","phone-number","phone-price");
